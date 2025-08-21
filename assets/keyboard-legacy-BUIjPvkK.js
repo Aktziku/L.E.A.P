d4f0a572398b2dee41a45a42e9c783114cfd4e66
Aktziku <lnasnober@gmail.com>
2025-08-21 16:15:19 +0800

    Updates

--- assets/keyboard-legacy-BUIjPvkK.js
+++ assets/keyboard-legacy-BUIjPvkK.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DZCram7l.js"],function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-DPaqE332.js"],function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
