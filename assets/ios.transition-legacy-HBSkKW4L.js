34c93b6e8d1469739aa453231d33b786a5000c35
Aktziku <lnasnober@gmail.com>
2025-10-30 15:28:06 +0800

    Updates

--- assets/ios.transition-legacy-HBSkKW4L.js
+++ assets/ios.transition-legacy-HBSkKW4L.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BR1QJm5Z.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-E3owUGra.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
